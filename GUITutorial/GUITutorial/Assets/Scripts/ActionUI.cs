f43dc053cfdd79f5687b1dc6b9d18dfc4a354260
LukeStanbridge <99298136+LukeStanbridge@users.noreply.github.com>
2022-08-03 12:19:28 +0930

    UI stuff

---
+++ GUITutorial/GUITutorial/Assets/Scripts/ActionUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ActionUI : MonoBehaviour
+{
+    public Action action;
+
+    [Header("Child Components")]
+    public Image icon;
+    public TextMeshProUGUI nameTag;
+    public TextMeshProUGUI descriptionTag;
+
+    Player player;
+    private void Start()
+    {
+        SetAction(action);
+    }
+
+    public void Init(Player p)
+    {
+        // store the player ref for use in our lambda function below
+        player = p;
+        // find the button wherever we've placed it in the prefab
+        // for more complicated types of prefabs with multiple buttons, we'd make this a public member
+        // and hook it up in the Unity editor
+        Button button = GetComponentInChildren<Button>();
+        if (button)
+            button.onClick.AddListener(() => { player.DoAction(action); });
+    }
+
+    public void SetAction(Action a)
+    {
+        action = a;
+        if (action == null) return;
+        if (nameTag)
+            nameTag.text = action.actionName;
+        if (descriptionTag)
+            descriptionTag.text = action.description;
+        if (icon)
+        {
+            icon.sprite = action.icon;
+            icon.color = action.color;
+        }
+    }
+}
---
+++ GUITutorial/GUITutorial/Assets/Scripts/Player.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public void DoAction(Action action)
+    {
+        Debug.Log("Doing " + action.actionName);
+    }
+}
