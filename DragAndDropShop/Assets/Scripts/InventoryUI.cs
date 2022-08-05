0b32946f8ad7dec6c27538ca01d5a2e20bd50b63
LukeStanbridge <99298136+LukeStanbridge@users.noreply.github.com>
2022-08-05 16:43:58 +0930

    game design doc

---
+++ DragAndDropShop/Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InventoryUI : MonoBehaviour
+{
+    public Inventory inventory;
+
+    public ShopItemUI itemPrefab;
+    public Slot slotPrefab;
+
+    Slot[] slots;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        // create a slot for each item
+        slots = new Slot[inventory.items.Length];
+        for (int i = 0; i < inventory.items.Length; i++)
+        {
+            slots[i] = Instantiate(slotPrefab, transform);
+            // create a shop item UI and feed the item to it
+            slots[i].itemUI = Instantiate(itemPrefab, slots[i].transform);
+            slots[i].itemUI.SetItem(inventory.items[i]);
+
+            // create a slot
+            slots[i].Init(this, i, slots[i].itemUI);
+        }
+    }
+}
--- OptionsMenu/Assets/Scripts/Settings.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Settings : MonoBehaviour
-{
-    public float musicVol;
-    public float soundFxVolume;
-}
